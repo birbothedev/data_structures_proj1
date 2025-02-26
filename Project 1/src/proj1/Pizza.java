f57a8e79f7ab5c9cb6e0ead40409077884fc56b9
ginafender <123966430+ginafender@users.noreply.github.com>
2025-02-26 13:46:42 -0500

    need to figure out the equals method

--- Project 1/src/proj1/Pizza.java
+++ Project 1/src/proj1/Pizza.java
@@ -1,20 +1,16 @@
 package proj1;
 
 import java.util.ArrayList;
+import java.util.List;
 
-public class Pizza {
+public class Pizza implements Comparable<Pizza> {
     public enum CrustType {
         PLAIN,
         BUTTER,
         GARLIC,
-        GARLICBUTTER,
+        GARLIC_BUTTER,
         CHEESE;
     }
-
-    private CrustType crust;
-    private ArrayList<String> toppings;
-    private SizeType size;
-
     public enum SizeType {
         SMALL,
         MEDIUM,
@@ -24,14 +20,25 @@ public class Pizza {
         PARTY;
     }
 
-    public Pizza(CrustType crust, ArrayList<String> toppings, SizeType size) {
-        this.crust = crust;
-        this.toppings = new ArrayList<>(toppings);
-        this.size = size;
+    private CrustType crust;
+    private ArrayList<String> toppings;
+    private final List<String> valid_toppings = List.of("cheese", "onion", "green pepper",
+            "ham", "pineapple", "pepperoni", "ground beef", "italian sausage", "anchovies");
+
+    private SizeType size;
+
+    public Pizza() {
+        this.crust = CrustType.PLAIN;
+        this.toppings = new ArrayList<>();
+        this.size = SizeType.SMALL;
     }
 
     public void addToppings(String topping){
-        toppings.add(topping);
+        if(valid_toppings.contains(topping)){
+            toppings.add(topping);
+        } else {
+            System.out.println("Please choose a valid topping.");
+        }
     }
 
     public ArrayList<String> getToppings() {
@@ -52,6 +59,35 @@ public class Pizza {
 
     public void setSize(SizeType size) {
         this.size = size;
+    }
+
+    @Override
+    public String toString(){
+        if(toppings.isEmpty()){
+            return "This pizza has a " + crust +
+                    "crust and the following toppings:\n" +
+                    "none";
+        } else {
+            return "This pizza has a " + crust +
+                    "crust and the following toppings:\n" +
+                    toppings;
+        }
+    }
+
+    @Override
+    public int compareTo(Pizza otherPizza){
+        int result = Integer.compare(this.toppings.size(), otherPizza.toppings.size());
+        if (result == 0){
+            result = this.crust.compareTo(otherPizza.crust);
+        }
+        return result;
+    }
+
+    @Override
+    public String equals(Pizza otherPizza){
+        if (this.toppings.size() == otherPizza.toppings.size()){
+            return "Pizzas are equal";
+        }
     }
 }
 
